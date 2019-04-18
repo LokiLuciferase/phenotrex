56be4ee1dd00dfa013fa9e6e057a36ee593bbe1a
phyden <patrick_hyden@gmx.at>
2019-04-18 10:02:18 +0200

    FIX replacing incomplete description of load_groups_file

--- pica/io/io.py
+++ pica/io/io.py
@@ -67,7 +67,8 @@ def load_phenotype_file(input_file: str, sign_mapping: Dict[str, int] = None) ->
 def load_groups_file(input_file: str, selected_rank: str = None) -> List[GroupRecord]:
     """
     Loads a .tsv file which contains group or taxid for each sample in the other training files.
-    Automatically classifies the [TODO missing text?]
+    Group-Ids may be ncbi-taxon-ids or arbitrary group names. Taxon-Ids are only used if a standard rank is selected,
+    otherwise user-specified group-ids are assumed.
 
     :param input_file: path to the file that is processed
     :param selected_rank: the standard rank that is selected (optional) if not set,
