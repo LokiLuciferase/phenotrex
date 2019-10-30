98e3a4d80589e3cae99c25345e7d77dabb881a7a
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2019-10-30 15:33:52 +0100

    add repr methods for dataclasses to prevent printing of features

--- pica/structure/records.py
+++ pica/structure/records.py
@@ -13,6 +13,9 @@ class GenotypeRecord:
     identifier: str
     features: List[str]
 
+    def __repr__(self):
+        return f"{self.identifier} n_features={len(self.features)}"
+
 
 @dataclass
 class PhenotypeRecord:
@@ -20,6 +23,9 @@ class PhenotypeRecord:
     identifier: str
     trait_name: str
     trait_sign: int
+
+    def __repr__(self):
+        return f"{self.identifier} trait({self.trait_name})={self.trait_sign}"
 
 
 @dataclass
@@ -29,8 +35,17 @@ class GroupRecord:
     group_name: str
     group_id: int
 
+    def __repr__(self):
+        return f"{self.identifier} group({self.group_name})={self.group_id}"
+
 
 @dataclass
 class TrainingRecord(GenotypeRecord, PhenotypeRecord, GroupRecord):
     """ TODO add docstring """
     pass
+
+    def __repr__(self):
+        gr_repr = GenotypeRecord.__repr__(self).split(' ')[1]
+        pr_repr = PhenotypeRecord.__repr__(self).split(' ')[1]
+        gro_repr = GroupRecord.__repr__(self).split(' ')[1]
+        return f"id={self.identifier} {' '.join([gr_repr, pr_repr, gro_repr])}"
