1e0e0bf317136ca58e8ff3a0fa2215a5829889a8
Roman Feldbauer <roman@feldbauer.org>
2020-04-22 10:35:36 +0200

    FIX Travis/Linux/AVX512 errors from PyTorch/MKLDNN

--- travis/install-pip.sh
+++ travis/install-pip.sh
@@ -2,7 +2,23 @@
 
 set -e
 
-echo "pip installing required python packages"
-pip install -r requirements/dev.txt
+# Alternative to "conda init bash"
+source "$HOME/miniconda/etc/profile.d/conda.sh"
+
+conda activate test
+hash -r
+
+# Work-around for a bug in PyTorch/MKLDNN on Linux and AVX512 systems:
+# Fetch the pytorch-nightly, until the official release.
+if [[ "$TRAVIS_OS_NAME" == 'linux' ]]; then
+  echo "install and upgrade PyTorch nightly"
+  conda install --yes pytorch cpuonly -c pytorch-nightly
+  echo "pip installing required python packages"
+  pip install -r requirements/dev.txt
+  python -c "import torch; print(f'PyTorch version = {torch.__version__}')"
+elif [[ "$TRAVIS_OS_NAME" == 'osx' ]]; then
+  echo "pip installing required python packages"
+  pip install -r requirements/dev.txt
+fi
 
 python --version
